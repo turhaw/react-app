7e4490d7957b16c2aa9f2d2a9ef2b287f857af57
turhaw <turhaw@gmail.com>
2021-07-07 21:30:40 +0400

    first commit

---
+++ src/components/edit/index.js
@@ -0,0 +1,53 @@
+function Edit() {
+
+const tranlate = {
+  1: "one",
+  2: "two",
+  3: "tree",
+  4: "four"
+}
+
+function print(a){
+  for (let key in tranlate){
+    if(key == a){
+      console.log(tranlate[key])
+    }
+  }
+}
+
+// function translate (i){
+//   switch (i) {
+//     case 1: console.log("one")
+//       break;
+//     case 2: console.log("two")
+//       break;
+//     case i%100 === 0: console.log("haryur")
+//       break;
+//   }
+// }
+function xuy(a) {
+
+
+}
+// function translate(a){
+//   if(a)
+//
+//
+// }
+
+//translate(100)
+
+for(let i=0 ; i< 10; i++){
+  console.log("xuy")
+}
+
+print(2)
+
+// kuku(8);
+  let i = "kku";
+  return (
+      <div>{i}</div>
+  )
+}
+
+export default Edit;
